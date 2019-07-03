6f47703a3f74d56d55724feefc386740777c0f68
Tyler Glisson <Tyler@tglisson.com>
2019-07-03 14:18:55 -0400

    add Delete functionality

--- index.js
+++ index.js
@@ -41,6 +41,23 @@ app.post('/task/add', function(req, res){
     });
 });
 
+app.post('/task/delete', function(req, res){
+    let tasksToDel = req.body.tasks;
+
+    client.lrange('tasks', 0, -1, function(err, tasks){
+        for(let i=0; i<tasks.length; i++){
+            if(tasksToDel.indexOf(tasks[i] > -1)){
+                client.lrem('tasks', 0, tasks[i], function(){
+                    if(err){
+                        console.log(err);
+                    }
+                })
+            }
+        }
+        res.redirect('/');
+    });
+});
+
 app.listen(3000);
 console.log('Server started on port 3000');
 
